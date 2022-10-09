16e13ea14ff4b0694efe1da47b19bd9db0009d82
AngelCruzPat <94579561+AngelCruzPat@users.noreply.github.com>
2022-10-09 14:36:40 -0500

    video 6

---
+++ java/net/itinajero/service/db/VacantesServiceJpa.java
@@ -0,0 +1,38 @@
+package net.itinajero.service.db;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import net.itinajero.model.Vacante;
+import net.itinajero.repository.VacantesRepository;
+import net.itinajero.service.IVacantesService;
+
+@Service
+@Primary
+public class VacantesServiceJpa implements IVacantesService {
+
+	@Autowired
+	private VacantesRepository vacantesRepo;
+	
+	@Override
+	public List<Vacante> buscarTodas() {
+		return vacantesRepo.findAll();
+	}
+
+	@Override
+	public Vacante buscarPorid(Integer idVacante) {
+		Optional<Vacante> optional = vacantesRepo.findById(idVacante);
+		
+		return null;
+	}
+
+	@Override
+	public void guardar(Vacante vacante) {
+		vacantesRepo.save(vacante);
+	}
+
+}
